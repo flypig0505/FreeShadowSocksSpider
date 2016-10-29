d52942af7cdb1ba310b5890030c1390df8615238
tianma8023 <1808931265@qq.com>
2016-10-29 16:11:26 +0800

    Add some comments.

--- src/tianma/ss/spider/craw/DefaultAccountCrawler.java
+++ src/tianma/ss/spider/craw/DefaultAccountCrawler.java
@@ -3,13 +3,25 @@ package tianma.ss.spider.craw;
 import org.apache.http.HttpHost;
 import org.apache.http.client.config.RequestConfig;
 
+/**
+ * Default Account Crawler.
+ * @author Tianma
+ *
+ */
 abstract public class DefaultAccountCrawler implements AccountCrawler{
 
+	/**
+	 * Provide a method of getting the instance of ShadowSocks Proxy.
+	 * @return
+	 */
 	protected RequestConfig getShadowSocksProxy() {
 		HttpHost proxy = new HttpHost("127.0.0.1", 1080, "http");
 		return RequestConfig.custom().setProxy(proxy).build();
 	}
 	
+	/**
+	 * Default proxyNeeded is false.
+	 */
 	@Override
 	public boolean proxyNeeded() {
 		return false;
--- src/tianma/ss/spider/util/TLog.java
+++ src/tianma/ss/spider/util/TLog.java
@@ -5,52 +5,92 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Log utility.
+ * 
  * @author Tianma
  *
  */
 public class TLog {
 
 	private static Logger sLogger;
-	
+
 	private TLog() {
-		
+
 	}
-	
+
+	/**
+	 * Init org.slf4j.Logger.
+	 * <p>
+	 * You must call it before calling other method.
+	 */
 	public static void init() {
 		if (sLogger == null)
 			initLogger();
 	}
-	
+
+	/**
+	 * Create the instance of org.slf4j.Logger.
+	 */
 	private static void initLogger() {
 		sLogger = LoggerFactory.getLogger("ShadowSocksSpiderLogger");
 	}
-	
+
 	public static void v(String msg) {
 		sLogger.trace(msg);
 	}
-	
+
+	/**
+	 * Log the message in debug level.
+	 * 
+	 * @param msg
+	 */
 	public static void d(String msg) {
 		sLogger.debug(msg);
 	}
-	
+
+	/**
+	 * Log the message in info level.
+	 * 
+	 * @param msg
+	 */
 	public static void i(String msg) {
 		sLogger.info(msg);
 	}
-	
+
+	/**
+	 * Log the message in warn level.
+	 * 
+	 * @param msg
+	 */
 	public static void w(String msg) {
 		sLogger.warn(msg);
 	}
-	
+
+	/**
+	 * Log the message in error level.
+	 * 
+	 * @param msg
+	 */
 	public static void e(String msg) {
 		sLogger.error(msg);
 	}
-	
+
+	/**
+	 * Log the throwable in error level. It can print the full-stack message of
+	 * the throwable object.
+	 * 
+	 * @param throwable
+	 */
 	public static void e(Throwable throwable) {
 		sLogger.error("", throwable);
 	}
-	
+
+	/**
+	 * Log the throwable with specific message.
+	 * @param msg
+	 * @param throwable
+	 */
 	public static void e(String msg, Throwable throwable) {
 		sLogger.error(msg, throwable);
 	}
-	
+
 }
